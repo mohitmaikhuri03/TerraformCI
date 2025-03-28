b98e55be14e9ee59bf4bcec2e6b1bbdadbb495bd
mohitmaikhuri03 <mkmaikhuri@gmail.com>
2025-03-28 23:14:02 +0530

    Update check.groovy

--- src/ci/terraform/template/check.groovy
+++ src/ci/terraform/template/check.groovy
@@ -10,12 +10,14 @@ def runpipeline(String terraformDir, String branch, String repoUrl, String crede
    Init = new init()
    Fmt = new fmt()
    Validate = new validate()
+   Lint = new lint()
 
   Clean.clean()
   Clone.clone(branch, repoUrl, credentialsId)
   Init.terraformInit(terraformDir)
   Fmt.terraformFormat(terraformDir)
   Validate.terraformValidate(terraformDir)
+  Lint.tflintScan(terraformDir)
   
 }
   
