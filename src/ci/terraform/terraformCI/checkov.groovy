34bcd295811c47dcfcf3046ee52f9b21ad1df14c
mohitmaikhuri03 <mkmaikhuri@gmail.com>
2025-03-29 00:23:13 +0530

    Update checkov.groovy

--- src/ci/terraform/terraformCI/checkov.groovy
+++ src/ci/terraform/terraformCI/checkov.groovy
@@ -3,7 +3,10 @@ package ci.terraform.terraformCI
 def terraformcheckov(String terraformDir) {
     dir(terraformDir) {
         sh '''
-            sudo pip install -q --upgrade checkov
+            if ! command -v checkov &> /dev/null; then
+                pip install --user -q --upgrade checkov
+            fi
+            export PATH=$HOME/.local/bin:$PATH
             checkov -d . --output json > checkov_report.json || true
         '''
         archiveArtifacts artifacts: 'checkov_report.json', fingerprint: true
