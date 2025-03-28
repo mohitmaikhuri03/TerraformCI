a90ea5d7c1357ab26ef7b9fdcc6da07dae1c6c97
mohitmaikhuri03 <mkmaikhuri@gmail.com>
2025-03-28 14:26:26 +0530

    Update check.groovy

--- src/CI/Terraform/Template/check.groovy
+++ src/CI/Terraform/Template/check.groovy
@@ -6,11 +6,13 @@ import CI.Terraform.TerraformCI.*
 def runpipeline(String terraformDir, String branch, String repoUrl, String credentialsId = null)
 {
   
-  Clean = new wsclean()
-  Clone = new gitclone()
-  init = new TerraformInit()
-  fmt = new Terraformfmt()
-  validate = new TerraformValidate()
+  
+
+  def clean = new wsclean()
+  def clone = new gitclone()
+  def init = new TerraformInit()
+  def fmt = new Terraformfmt()
+  def validate = new TerraformValidate()
 
   Clean.clean()
   Clone.clone(branch, repoUrl, credentialsId)
