c085f2ca9a597ec13731032cebd0ad6579e90d16
mohitmaikhuri03 <mkmaikhuri@gmail.com>
2025-03-28 12:43:13 +0530

    Create CI.groovy

---
+++ src/CI/Terraform/Template/CI.groovy
@@ -0,0 +1,23 @@
+package CI.Terraform.Tmeplate
+
+import CI.Terraform.Common.*
+import CI.Terraform.TerraformCI.*
+
+def runpipeline(String terraformDir, String branch, String repoUrl, String credentialsId = null)
+{
+  
+  Clean = new wsclean()
+  Clone = new gitclone()
+  init = new TerraformInit()
+  fmt = new TerraformFmt()
+  validate = new Terrafromvalidate()
+
+  Clean.clean()
+  Clone.clone(branch, repoUrl, credentialsId)
+  init.terraformInit(terraformDir)
+  fmt.terraformFormat(terraformDir)
+  validate.terraformValidate(terraformDir)
+  
+}
+  
+  
