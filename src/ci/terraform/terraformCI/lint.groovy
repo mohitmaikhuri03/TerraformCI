356da5e4bfb805a72c5a587e2169db96e9c07b5c
mohitmaikhuri03 <mkmaikhuri@gmail.com>
2025-03-28 23:47:08 +0530

    Update lint.groovy

--- src/ci/terraform/terraformCI/lint.groovy
+++ src/ci/terraform/terraformCI/lint.groovy
@@ -4,8 +4,8 @@ def tflintScan(String terraformDir) {
     dir(terraformDir) {
         sh """ 
             curl -s https://raw.githubusercontent.com/terraform-linters/tflint/master/install_linux.sh | bash
-            tflint --format html > tflint_report.html || true
+            tflint --format json > tflint_report.json || true
         """
-        archiveArtifacts artifacts: 'tflint_report.html', fingerprint: true
+        archiveArtifacts artifacts: 'tflint_report.json', fingerprint: true
     }
 }
