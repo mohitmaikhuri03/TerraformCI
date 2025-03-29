ba3ddcf605ba60715b71b2022af1a650827590fc
mohitmaikhuri03 <mkmaikhuri@gmail.com>
2025-03-29 12:32:31 +0530

    Update cost.groovy

--- src/ci/terraform/terraformCI/cost.groovy
+++ src/ci/terraform/terraformCI/cost.groovy
@@ -2,15 +2,15 @@ package ci.terraform.terraformCI
 
 def terraformCostEstimate(String terraformDir, String INFRACOST_API_KEY) {
     dir(terraformDir) {
-        sh """
-            if ! command -v infracost &> /dev/null; then
-                curl -fsSL https://raw.githubusercontent.com/infracost/infracost/master/scripts/install.sh | sh
-            fi
-            
-            export INFRACOST_API_KEY=${INFRACOST_API_KEY}
-            
-            infracost breakdown --path . --format json > infracost_report.json || true
-        """
-        archiveArtifacts artifacts: 'infracost_report.json', fingerprint: true
+        withEnv(["INFRACOST_API_KEY=${INFRACOST_API_KEY}"]) {  
+            sh """
+                if ! command -v infracost >/dev/null 2>&1; then
+                    curl -fsSL https://raw.githubusercontent.com/infracost/infracost/master/scripts/install.sh | sh
+                fi
+                
+                infracost breakdown --path . --format html > infracost_report.html || true
+            """
+        }
+        archiveArtifacts artifacts: 'infracost_report.html', fingerprint: true
     }
 }
