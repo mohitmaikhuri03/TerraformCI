af1096d2f6bee2f032e28046644fa6f4089f8b3d
mohitmaikhuri03 <mkmaikhuri@gmail.com>
2025-04-02 14:30:58 +0530

    Update checkov.groovy

--- src/ci/terraform/terraformCI/checkov.groovy
+++ src/ci/terraform/terraformCI/checkov.groovy
@@ -1,3 +1,18 @@
+// package ci.terraform.terraformCI
+
+// def terraformcheckov(String terraformDir) {
+//     dir(terraformDir) {
+//         sh '''
+//             if ! command -v checkov &> /dev/null; then
+//                 pip install --user -q --upgrade checkov
+//             fi
+//             export PATH=$HOME/.local/bin:$PATH
+//             checkov -d . --output json > checkov_report.json || true
+//         '''
+//         archiveArtifacts artifacts: 'checkov_report.json', fingerprint: true
+//     }
+// }
+
 package ci.terraform.terraformCI
 
 def terraformcheckov(String terraformDir) {
@@ -7,8 +22,12 @@ def terraformcheckov(String terraformDir) {
                 pip install --user -q --upgrade checkov
             fi
             export PATH=$HOME/.local/bin:$PATH
-            checkov -d . --output json > checkov_report.json || true
+        
+            checkov -d . --output cli | tee checkov_report.txt
+
+            PASSED_COUNT=$(grep -oP 'Passed checks: \\K\\d+' checkov_report.txt)
+            echo " Total Passed Checks: $PASSED_COUNT"
         '''
-        archiveArtifacts artifacts: 'checkov_report.json', fingerprint: true
+        archiveArtifacts artifacts: 'checkov_report.txt', fingerprint: true
     }
 }
